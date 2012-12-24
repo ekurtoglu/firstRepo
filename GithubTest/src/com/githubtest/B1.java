1760a8b40ea0f852a4dbbb93a7f6e924b8ea6db7
Engin Kurtoğlu <kurtoglu.engin@gmail.com>
2012-12-24 22:48:47 +0200

    master

--- GithubTest/src/com/githubtest/B1.java
+++ GithubTest/src/com/githubtest/B1.java
@@ -2,7 +2,13 @@ package com.githubtest;
 
 public class B1 {
 	public void deneme() {
+		
 		System.out.println("B1");
+		
+		long l = System.currentTimeMillis();
+		
+		
+		System.out.println(l);
 	}
 
 }
