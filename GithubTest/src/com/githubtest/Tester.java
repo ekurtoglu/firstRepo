9943b3165964120caf86bfaf471fa701d17dcdce
Engin Kurtoğlu <kurtoglu.engin@gmail.com>
2012-12-24 23:12:20 +0200

    s<afdasz

--- GithubTest/src/com/githubtest/Tester.java
+++ GithubTest/src/com/githubtest/Tester.java
@@ -6,12 +6,14 @@ public class Tester {
 		
 		
 		System.out.println("Test 2");
+		long l = 123L;
 		System.out.println("New Line");
 		
 		System.out.println("asdfasdf");
 		System.out.println("Test 4");
 		
 		//added in b1
+		System.out.println(l);
 		}
 	}
 }
