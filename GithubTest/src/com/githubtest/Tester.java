96a09c7f72ecf24b71d3374ec5b3c76acf4fff7c
Engin Kurtoğlu <kurtoglu.engin@gmail.com>
2012-12-24 22:43:13 +0200

    master if

--- GithubTest/src/com/githubtest/Tester.java
+++ GithubTest/src/com/githubtest/Tester.java
@@ -2,11 +2,14 @@ package com.githubtest;
 
 public class Tester {
 	public static void main(String[] args) {
+		if(System.currentTimeMillis() > 0) {
+			
 		System.out.println("Test 2");
 		//master
 		System.out.println("New Line");
 		System.out.println("Test 4");
 		
 		//added in b1
+		}
 	}
 }
