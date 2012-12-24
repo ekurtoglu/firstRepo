2e774edf8449efec0a062c61d83a4208fd0d0bd0
Engin Kurtoğlu <kurtoglu.engin@gmail.com>
2012-12-24 23:14:16 +0200

    sfsdf

--- GithubTest/src/com/githubtest/Tester.java
+++ GithubTest/src/com/githubtest/Tester.java
@@ -7,7 +7,6 @@ public class Tester {
 		
 		
 		System.out.println("Test 2");
-		long l = 123L;
 		System.out.println("New Line");
 		
 		System.out.println("asdfasdf");
@@ -15,7 +14,6 @@ public class Tester {
 		System.out.println("Test 4");
 		
 		//added in b1
-		System.out.println(l);
 		}
 	}
 }
