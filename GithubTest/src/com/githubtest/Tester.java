aa0aa5c7462497285fa7128a8024d2017c35f947
Engin Kurtoğlu <kurtoglu.engin@gmail.com>
2012-12-24 23:08:59 +0200

    asdf

--- GithubTest/src/com/githubtest/Tester.java
+++ GithubTest/src/com/githubtest/Tester.java
@@ -3,19 +3,15 @@ package com.githubtest;
 public class Tester {
 	public static void main(String[] args) {
 		if(System.currentTimeMillis() > 0) {
-		if(System.currentTimeMillis() > 0) {
-			
+		
+		
 		System.out.println("Test 2");
-		long newLong = System.currentTimeMillis();
-		//master
 		System.out.println("New Line");
-		long l = System.currentTimeMillis();
-		System.out.println(l);
-		}
+		
+		System.out.println("asdfasdf");
 		System.out.println("Test 4");
 		
 		//added in b1
-		System.out.println(newLong);
 		}
 	}
 }
