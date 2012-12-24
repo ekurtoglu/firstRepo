7c614e82e1262400fba2c7c5c6fe0a509ede29b3
Engin Kurtoğlu <kurtoglu.engin@gmail.com>
2012-12-24 22:51:54 +0200

    s

--- GithubTest/src/com/githubtest/B1.java
+++ GithubTest/src/com/githubtest/B1.java
@@ -2,22 +2,12 @@ package com.githubtest;
 
 public class B1 {
 	public void deneme() {
-<<<<<<< HEAD
-		if (1 == 1) {
-=======
-		
->>>>>>> refs/heads/master
 		System.out.println("B1");
 		
-<<<<<<< HEAD
-		
-		}
-=======
 		long l = System.currentTimeMillis();
 		
 		
 		System.out.println(l);
->>>>>>> refs/heads/master
 	}
 
 }
