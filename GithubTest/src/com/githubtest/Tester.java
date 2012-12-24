e74f662f3bda9060654fa538b11107ab945d63a5
Engin Kurtoğlu <kurtoglu.engin@gmail.com>
2012-12-24 23:18:25 +0200

    qwe

--- GithubTest/src/com/githubtest/Tester.java
+++ GithubTest/src/com/githubtest/Tester.java
@@ -3,14 +3,12 @@ package com.githubtest;
 public class Tester {
 	public static void main(String[] args) {
 		if(System.currentTimeMillis() > 0) {
-			if(System.currentTimeMillis() > 0) {
 		
 		
 		System.out.println("Test 2");
 		System.out.println("New Line");
 		
 		System.out.println("asdfasdf");
-			}
 		System.out.println("Test 4");
 		
 		//added in b1
